111150207976439c1e4319594d1a22ba6f51903b
JeilJeong <42158390+JeilJeong@users.noreply.github.com>
2021-12-11 21:01:20 +0900

    boj by java

--- boj/문자열/java/Main_2675.java
+++ boj/문자열/java/Main_2675.java
@@ -1,3 +1,25 @@
-public class Main_2675 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main_2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = null;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());
+            String str = st.nextToken();
+            for (int j = 0; j < str.length(); j++) {
+                for (int p = 0; p < k; p++) {
+                    System.out.printf("%c", str.charAt(j));
+                }
+            }
+            System.out.println();
+        }
+        br.close();
+    }
 }
