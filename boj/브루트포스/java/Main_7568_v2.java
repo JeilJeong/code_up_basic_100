87062a681747a8c546deb5585c19cdf92c6a62f8
JeilJeong <42158390+JeilJeong@users.noreply.github.com>
2022-02-01 11:17:40 +0900

    boj

--- boj/브루트포스/java/Main_7568_v2.java
+++ boj/브루트포스/java/Main_7568_v2.java
@@ -1,6 +1,9 @@
+// ref : https://st-lab.tistory.com/99
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main_7568_v2 {
     public static void main(String[] args) throws IOException {
@@ -8,8 +11,25 @@ public class Main_7568_v2 {
 
         int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
+        int[][] arr = new int[n][2];
 
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            int rank = 1;
+
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    continue;
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    rank++;
+                }
+            }
+            System.out.print(rank + " ");
         }
         br.close();
     }
